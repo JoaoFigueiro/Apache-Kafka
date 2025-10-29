5cd988c5216081004d5705bd35e9ace720bfaa94
JoaoFigueiro <joao.l.comiran@gmail.com>
2025-10-29 16:18:04 -0300

    feat: wikimedia changer producer

--- kafka-producer-wikimedia/src/main/java/org/WikimediaChangerProducer.java
+++ kafka-producer-wikimedia/src/main/java/org/WikimediaChangerProducer.java
@@ -1,5 +1,38 @@
 package org;
 
-public class WikimediaChangerProducer {
+import com.launchdarkly.eventsource.EventSource;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.beans.EventHandler;
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class WikimediaChangerProducer {
+    public static void main(String[] args) throws InterruptedException {
+
+        String bootstrapServer = "localhost:9092";
+
+        Properties properties = new Properties();
+
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        String topic = "wikimedia.recentchange";
+
+        EventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
+        String url = "https://stram.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+        EventSource eventSource = builder.build();
+
+        eventSource.start();
+
+        TimeUnit.MINUTES.sleep(10);
+
+    }
 }
